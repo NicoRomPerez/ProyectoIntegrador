256f9e58233ea6ec789b2b4e0b8f05c3d0dcf2b1
Bren-Mont <bremonterosa@gmail.com>
2022-10-25 18:25:54 -0500

    Registro con validaciones

--- proyectointegrador/src/Components/Auth/register/Register.js
+++ proyectointegrador/src/Components/Auth/register/Register.js
@@ -7,31 +7,64 @@ import "./../auth.scss";
 import "./Register.scss";
 import {validateEmail, validatePassword, validateRePassword} from './../utils'
 const Register = () => {
-  return (
-    <div className='auth'>
-        <Header className="header"></Header>
-        <Body className='container'> 
-          <div className='card-login'>
-            <h1 className='tittle'>
-              Crear cuenta
-            </h1>
-            <div className='full-name'>
-              <Input label='Nombre'/>
-              <Input label='Apellido'/>
-            </div>
-              <Input label='Correo electrónico'/>
-              <Input label='Contraseña'/>
-              <Input label='Confirmar contraseña'/>
-              <div className='container-button'>
-                <button className='buttonPrimary'>
-                  Crear cuenta
-                </button>
-              </div>
-            </div>
-          </Body>
-          <Footer className="footer"></Footer>
-    </div>
-  )
-}
+  const [password, setPassword]= useState("");
+  const [rePassword, setRePassword]= useState("");
+  const [email, setEmail]= useState("");
+  const [errorsMsg, setErrorsMsg] = useState({});
 
-export default Register
+  const onRegister = (e) => {
+    e.preventDefault();
+
+    validateEmail(email, setErrorsMsg);
+    validatePassword(password, setErrorsMsg)
+    validateRePassword(password, rePassword,setErrorsMsg)
+
+    if (errorsMsg.email || errorsMsg.password || errorsMsg.rePassword) {
+      return
+    } 
+
+    console.log('Guardado con exito')
+  }
+  return (
+    <div className="auth">
+      <Header className="header"></Header>
+      <Body className="container">
+        <div className="card-login">
+          <h1 className="tittle">Crear cuenta</h1>
+          <form onSubmit={onRegister}>
+            <div className="full-name">
+              <Input label="Nombre" />
+              <Input label="Apellido" />
+            </div>
+            <Input label="Correo electrónico" value={email} type="email" onChange={(e) => setEmail(e.target.value)} error={errorsMsg.email}/>
+            <Input
+              label="Contraseña"
+              type="password"
+              required={true}
+              minLength={6}
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+              error={errorsMsg.password}
+            />
+            <Input
+              label="Confirmar contraseña"
+              type="password"
+              required={true}
+              minLength={6}
+              value={rePassword}
+              onChange={(e) => setRePassword(e.target.value)}
+              error={errorsMsg.rePassword}
+            />
+            <div className="container-button">
+              <button className="buttonPrimary" onClick={onRegister} >Crear cuenta</button>
+            </div>
+          </form>
+
+        </div>
+      </Body>
+      <Footer className="footer"></Footer>
+    </div>
+  );
+};
+
+export default Register;
