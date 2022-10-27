1d9bc391f2d35e3009f7cf70d0059d54bc3451da
NataliaOrtizMa <nataliaortizma@gmail.com>
2022-10-26 19:31:04 -0500

    Agregar bloque categoria y lista al home

--- proyectointegrador/src/Components/Home.js
+++ proyectointegrador/src/Components/Home.js
@@ -2,6 +2,8 @@ import Footer from './Footer/Footer'
 import Header from './Header/Header'
 import '../App.scss'
 import Body from './Body/Body'
+import CategoriesBlock from './Body/CategoriesBlock/CategoriesBlock'
+import ListBlock from './Body/ListBlock/ListBlock'
 
 const Home = (log) => {
   const a = log;
@@ -11,7 +13,10 @@ const Home = (log) => {
     <div>
         <Header className="header" log={a}></Header>
         <Body>
-          
+        <Body className="body">
+          <CategoriesBlock></CategoriesBlock>
+          <ListBlock></ListBlock>
+        </Body>
         </Body>
         <Footer className="footer"></Footer>
     </div>
